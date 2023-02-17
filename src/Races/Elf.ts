38663b0bc99add1aedbf0f969e2d66b9a08d329a
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2023-02-17 16:43:50 -0300

    Requisito 07

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -3,6 +3,7 @@ import Race from './Race';
 export default class Elf extends Race {
   private _maxLifePoints: number;
   private static instancesElfCount = 0;
+  
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
     this._maxLifePoints = 99;
